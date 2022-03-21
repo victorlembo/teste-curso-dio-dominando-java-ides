8e26b9689fa2e8e3cdc5446c34d456425b180602
Victor Lembo <victordovalle88@gmail.com>
2022-03-21 14:42:29 -0300

    First Commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+
+		System.out.println(gato);
+		
+	
+	}
+
+}
